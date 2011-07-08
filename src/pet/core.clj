8f2a0d020ed7e3c5f6472d0b7c977f2e4018de3a
unknown <khebsgaard@.corp.ebay.com>
2011-07-08 16:21:32 +0200

    started using commandline stuff, but not working yet

--- src/pet/core.clj
+++ src/pet/core.clj
@@ -1,5 +1,6 @@
 (ns pet.core
   (:require [clojure.contrib.sql :as sql])
+  (:require [clojure.contrib.command-line :as cmd])
   (:gen-class))
 
 
@@ -11,22 +12,33 @@
 (. Class (forName "org.sqlite.JDBC")) ; Initialize the JDBC driver
 
 (defn db-create 
- "Creates the table for this model"
+  "Creates the table for this model"
   []
   (sql/create-table
-   :something
-   [:id :int "PRIMARY KEY"]
-   [:name "varchar(32)"]))
+    :something
+    [:id :int "PRIMARY KEY"]
+    [:name "varchar(32)"]))
 
 (defn db-insert
   "inserts to the database" 
-   []
-   (sql/do-commands "INSERT INTO something(id,name) VALUES (1,'khebbie')"))
+  []
+  (sql/do-commands "INSERT INTO something(name) VALUES ('khebbie')"))
 
-(defn -main []
+(defn -main [& args]
+  (cmd/with-command-line args
+                         "Command line demo"
+                         [[install? b? "Choose if the database install should be run"]
+                          remaining]
+                         (if (or install? false)
+                           (sql/with-connection
+                             db
+                             (sql/transaction
+                               (db-insert))))
 
-(sql/with-connection
- db
- (sql/transaction
-  (db-insert))))
+                         )
+
+  (sql/with-connection
+    db
+    (sql/transaction
+      (db-insert))))
 
